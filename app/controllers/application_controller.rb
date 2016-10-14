99fad2fba923a00412091d7476dfd817a6ded6c1
Miles Butler <mbutle35@uncc.edu>
2016-10-14 19:09:14 +0000

    Fixing @article.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ def new
 end
 
 def create
-  @article = Article.new(params[:article])
+  @article = Article.new(params.require(:article).permit(:title, :text))
  
   @article.save
   redirect_to @article
