072f42214db67413adcca240ffb8e876d765619b
Miles Butler <mbutle35@uncc.edu>
2016-10-14 19:18:14 +0000

    Index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
 def new
 end
 
